604b269c7f2924675306d5f8b1d78b8585c94d28
Abhishek Suman <iabhishek.suman1993@gmail.com>
2024-11-25 18:45:06 -0600

    training successful!

--- notebooks/data_cleaning/train.py
+++ notebooks/data_cleaning/train.py
@@ -1,40 +1,49 @@
 import os
 import pandas as pd
+import json
+import argparse
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-def load_data():
+def load_data(args):
     # Assuming the environment variables are set by the SageMaker XGBoost estimator
-    train_features_path = os.environ['SM_CHANNEL_TRAIN_FEATURES']
-    train_labels_path = os.environ['SM_CHANNEL_TRAIN_LABELS']
+    print('hereee')
+    print(args)
+    # test_date = os.environ['test_date']
+    test_date = args.test_date
+    data_path = os.environ['SM_CHANNEL_TRAIN_FEATURES']
+    # train_labels_path = os.environ['SM_CHANNEL_TRAIN_LABELS']
 
     # Load the datasets
-    train_features = pd.read_parquet(train_features_path)
-    train_labels = pd.read_parquet(train_labels_path)
+    data = pd.read_parquet(data_path).fillna(0)
+    train_X = data.loc[:test_date, ].drop(columns='category')
+    train_y = data.loc[:test_date, 'category'] # ['category']
 
-    return train_features, train_labels
+    test_X = data.loc[test_date:, ].drop(columns='category')
+    test_y = data.loc[test_date:, 'category'] # ['category']
 
-def train_model(train_features, train_labels):
+    return train_X, train_y, test_X, test_y 
+
+def train_model(train_X, train_y, test_X, test_y, args):
     # Splitting the dataset for training and validation
-    X_train, X_val, y_train, y_val = train_test_split(train_features, train_labels, test_size=0.2, random_state=42)
+    # X_train, X_val, y_train, y_val = train_test_split(train_features, train_labels, test_size=0.2, random_state=42)
 
     # Creating DMatrix for XGBoost
-    dtrain = xgb.DMatrix(data=X_train, label=y_train)
-    dval = xgb.DMatrix(data=X_val, label=y_val)
+    dtrain = xgb.DMatrix(data=train_X, label=train_y)
+    # dval = xgb.DMatrix(data=X_val, label=y_val)
+    dtest = xgb.DMatrix(data=test_X, label=test_y)
 
     # Parameters for XGBoost
+    # params = os.environ['hyperparameters']
     params = {
-        'objective': 'multi:softmax',
-        'num_class': 3,
-        'max_depth': 6,
-        'eta': 0.3,
-        'eval_metric': 'mlogloss',
+        'objective': args.objective,
+        'num_class': args.num_class,                  # Since you have three classes A, B, C
     }
-    num_boost_round = 100
+    num_boost_round = args.num_round
 
     # Training the model
-    bst = xgb.train(params, dtrain, num_boost_round, evals=[(dval, 'validation')], early_stopping_rounds=10)
+    bst = xgb.train(params, dtrain, num_boost_round, evals=[(dtest, 'test')], early_stopping_rounds=10)
 
     return bst
 
@@ -51,7 +60,20 @@ def save_model(model, model_path):
     model.save_model(model_path)
 
 if __name__ == '__main__':
-    train_features, train_labels = load_data()
-    model = train_model(train_features, train_labels['label'])  # Adjust based on your label column name
-    evaluate_model(model, train_features, train_labels['label'])  # Adjust based on your label column name
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--objective', type=str, default='multi:softmax',
+                        help='objective function')
+    parser.add_argument('--num_class', type=int, default=3,
+                        help='Number of classes')
+    parser.add_argument('--num_round', type=int, default=100,
+                        help='number of boosting rounds')
+    parser.add_argument('--test_date', type=str, default='2024-10-01',
+                        help='test date cut off')
+
+    # Parse arguments
+    args = parser.parse_args()
+    train_X, train_y, test_X, test_y = load_data(args)
+    model = train_model(train_X, train_y, test_X, test_y, args)  # Adjust based on your label column name
+    evaluate_model(model, test_X, test_y)  # Adjust based on your label column name
     save_model(model, os.environ['SM_MODEL_DIR'] + '/xgboost-model')
